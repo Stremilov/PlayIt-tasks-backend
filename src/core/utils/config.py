df19af6de0866338aa843762c83f855187806a9a
Lef <116380867+Stremilov@users.noreply.github.com>
2025-02-04 03:20:37 +0300

    fix

--- src/core/utils/config.py
+++ src/core/utils/config.py
@@ -11,7 +11,7 @@ DB_HOST = os.getenv("DATABASE_HOST", "localhost")
 DB_NAME = os.getenv("DATABASE_NAME", "postgres")
 DB_USER = os.getenv("DATABASE_USER", "postgres")
 DB_PORT = os.getenv("DATABASE_PORT", "5432")
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24

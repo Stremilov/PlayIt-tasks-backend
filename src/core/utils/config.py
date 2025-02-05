894c9ef55fdebd06512387e9b7b212670b2b13fe
unknown <maks-kandikov@yandex.ru>
2025-02-06 02:55:22 +0300

    конфиг под Redis

--- src/core/utils/config.py
+++ src/core/utils/config.py
@@ -16,5 +16,11 @@ DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_POR
 SECRET_KEY = os.getenv("SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
+CACHE_KEY = os.getenv("CACHE_KEY", "tasks:all")  # Ключ для хранения данных кеша
+CACHE_EXPIRE = int(os.getenv("CACHE_EXPIRE", 21600))  # Время жизни кеша: 6 часов = 6 * 3600 секунд
+
 UPLOAD_FOLDER = Path("uploads/images")
 UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)

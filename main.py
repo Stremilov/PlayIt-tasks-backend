3d04724af2c58b562384bd72b790649ed14e9926
Lef <116380867+Stremilov@users.noreply.github.com>
2025-02-13 19:49:18 +0300

    fixed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uvicorn
 import logging
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.api.routers import all_routers
 from src.core.utils.config import settings
@@ -14,7 +15,16 @@ logging.basicConfig(
 
 
 app = FastAPI(root_path="/playit/tasks")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 for router in all_routers:
     app.include_router(router)

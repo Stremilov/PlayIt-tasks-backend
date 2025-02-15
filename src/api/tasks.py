fe71fa630518e612ce025383913df8b978ec8a74
Lef <116380867+Stremilov@users.noreply.github.com>
2025-02-15 19:56:36 +0300

    update

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from typing import Optional
 
 from aiohttp import ClientSession, FormData
 from fastapi import APIRouter, Request, Form, UploadFile, File, HTTPException
@@ -38,29 +39,47 @@ async def parse_all_tasks(request: Request):
 
 @router.post(
     path="/create",
+    tags=["Tasks"],
     summary="Создать задание",
-    description="Создаёт задание и отправляет его модератору с фото.",
+    description="Создаёт задание и отправляет его модератору с файлом.",
     responses=base_bad_response_for_endpoints_of_task
 )
 async def create_task(
-    task_id: int = Form(..., description="ID задания"),
-    user_id: int = Form(..., description="ID пользователя"),
-    value: str = Form(..., description="Количество баллов"),
-    photo: UploadFile = File(..., description="Фото для задания")
+        task_id: int = Form(..., description="ID задания"),
+        user_id: int = Form(..., description="ID пользователя"),
+        value: str = Form(..., description="Количество баллов"),
+        text: Optional[str] = Form(default=None, description="Текст выполненного задания"),
+        file: Optional[UploadFile] = File(default=None, description="Файл для задания")
 ):
     """
     Этот эндпоинт принимает данные задания и отправляет его модератору через Telegram Bot API.
     """
     logging.info("Данные приняты")
-    result = await send_task_to_moderator(task_id, user_id, value, photo)
+
+    if isinstance(file, str) and file == "":
+        file = None
+
+    # Передаем параметры в правильном порядке
+    result = await send_task_to_moderator(task_id, user_id, value, text, file)
+
     return {"status": result}
 
 
-async def send_task_to_moderator(task_id: int, user_id: int, value: str, photo: UploadFile):
-    url = f"https://api.telegram.org/bot{settings.bot.TELEGRAM_BOT_TOKEN}/sendPhoto"
+async def send_task_to_moderator(
+        task_id: int, user_id: int, value: str, text: Optional[str] = None, file: Optional[UploadFile] = None
+):
+    """
+    Функция отправляет задание модератору.
+    """
+    url = f"https://api.telegram.org/bot{settings.bot.TELEGRAM_BOT_TOKEN}/sendMessage"
 
-    message = (f"Новое задание от пользователя:\n\n"
-               f"Количество баллов: {value}")
+    message = (
+        f"Новое задание от пользователя:\n\n"
+        f"Количество баллов: {value}"
+    )
+
+    if text:
+        message += f"\nТекст: {text}"
 
     keyboard = {
         "inline_keyboard": [
@@ -71,9 +90,13 @@ async def send_task_to_moderator(task_id: int, user_id: int, value: str, photo: 
 
     form_data = FormData()
     form_data.add_field('chat_id', str(settings.bot.MODERATOR_CHAT_ID))
-    form_data.add_field('caption', message)
+    form_data.add_field('text', message)
     form_data.add_field('reply_markup', json.dumps(keyboard))
-    form_data.add_field('photo', photo.file, filename=photo.filename, content_type=photo.content_type)
+
+    # Если есть файл, отправляем фото/видео
+    if file:
+        url = f"https://api.telegram.org/bot{settings.bot.TELEGRAM_BOT_TOKEN}/sendPhoto"
+        form_data.add_field('photo', file.file, filename=file.filename, content_type=file.content_type)
 
     async with ClientSession() as session:
         async with session.post(url, data=form_data) as response:

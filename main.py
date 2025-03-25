7dd21d4aabb2589cca3888314b41fb3bdef486af
cl7paBka <maks-kandikov@yandex.ru>
2025-03-25 20:48:40 +0300

    Удалил тот локалхост, потом потру коммиты

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://172.20.10.9:5173" # localhost Фронта
     ],
     allow_credentials=True,
     allow_methods=["*"],

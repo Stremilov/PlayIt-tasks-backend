8137196fccc682ef127a8b33cd38e9bc12103a4b
cl7paBka <maks-kandikov@yandex.ru>
2025-03-25 20:08:58 +0300

    Добавляю локалхост нашего фронтенд разработчика

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "http://172.20.10.9:5173" # localhost Фронта
     ],
     allow_credentials=True,
     allow_methods=["*"],
